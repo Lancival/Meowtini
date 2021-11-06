0d4bf84932e5edecf1d5bf2e283be68544a4d35b
Do3naut <51458829+Do3naut@users.noreply.github.com>
2021-11-06 02:32:34 -0700

    Drink correctness detection
    the Drink class was created as a placeholder in order to start implementing drink detection logic

--- Assets/Scripts/CustomerController.cs
+++ Assets/Scripts/CustomerController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class CustomerController : MonoBehaviour
 {
+    [Header("Timing")]
     [SerializeField] int startingTimer = 60; // In seconds
-    public int counter;
     public int timer;
+
+    [Header("Error Calculation")]
+    [SerializeField] int errorThreshold = 30;
+
 
     [SerializeField] SpriteRenderer timerIcon;
     private Color iconColor = Color.green;
@@ -17,17 +21,75 @@ public class CustomerController : MonoBehaviour
     {
         timer = startingTimer;
         timerIcon.color = iconColor;
-        InvokeRepeating("HandleTimer", 0, 1);
+        InvokeRepeating("HandleTimer", 0, 1); 
     }
 
-
-    // Update is called once per frame
-    void Update()
+    // Judges if the candidate matches the target sufficiently
+    bool EvaluateDrink(Drink candidate, Drink target)
     {
+        /*
+         * This code has not been tested, but in theory this should be a functional method to account for error.
+         */
+        int errorValue = 0;
+
+        // Compare candidate's topping list to the target, adding errorValue when discrepancies occur
+        foreach(string topping in candidate.toppings.Keys)
+        {
+            if (target.toppings.ContainsKey(topping))
+            {
+                int dif = candidate.toppings[topping] - target.toppings[topping];
+                errorValue += Mathf.Abs(dif) * 2;  // Add error score of 2 for each extra/missing topping
+
+            } else
+            {
+                int count = candidate.toppings[topping];
+                errorValue += Mathf.Abs(count) * 2;
+            }
+        }
+        // Accounts for missing toppings from the candidate
+        foreach (string topping in target.toppings.Keys)
+        {
+            if (!candidate.toppings.ContainsKey(topping))
+            {
+                int count = target.toppings[topping];
+                errorValue += Mathf.Abs(count) * 2;
+            }
+        }
+
+
+        // Liquid check
+        float tolerance = 20f * Mathf.Pow(0.8f, 0);  // 0 should be set to the difficulty variable in the future
         
+        foreach (string liquid in candidate.liquids.Keys)
+        {
+            if (target.toppings.ContainsKey(liquid))
+            {
+                float dif = Mathf.Abs(candidate.liquids[liquid] - target.liquids[liquid]);
+                if (dif > tolerance)
+                {
+                    dif -= tolerance;
+                    errorValue += Mathf.CeilToInt(dif / tolerance);  // Gain 1 errorValue for each tolerance away from the target answer
+                }
+            }
+            else
+            {
+                errorValue += 5;  // Gain 5 errorValue for adding wrong liquid
+            }
+        }
+        // Accounts for missing liquids from the candidate
+        foreach (string liquid in target.liquids.Keys)
+        {
+            if (!candidate.liquids.ContainsKey(liquid))
+            {
+                errorValue += 5;  // Gain 5 errorValue for missing a liquid
+            }
+        }
+
+
+        return errorValue < errorThreshold;
     }
 
-    // Updates the game accordingly 
+    // Updates the time
     void HandleTimer()
     {
         timer--;
@@ -57,7 +119,7 @@ public class CustomerController : MonoBehaviour
         {
             Debug.Log("ORDER FAILED");
             timerIcon.color = Color.black;
-            GameManager.Instance.DecreaseSatisfaction(1);
+            GameManager.Instance.DrinkDenied(1);  // Calls GameManager to decrease satisfaction and reset the combo
             GameObject.Destroy(gameObject);
         }
     }
