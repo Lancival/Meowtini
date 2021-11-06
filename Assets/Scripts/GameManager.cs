1556d36f6c1df1a8d6298894f3084693b132feff
Peter Sutarjo <peter.sutarjo@gmail.com>
2021-11-06 00:07:19 -0700

    Added combo counter + satisfaction level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,6 +15,14 @@ public class GameManager : GenericSingleton<GameManager>
         [SerializeField] private float timer = 0.0f;
         [Tooltip("How many seconds have passed since the beginnig of the game")]
         [SerializeField] private int secondsElapsed;
+    
+    [Header("Combo counter")]
+        [Tooltip("Drinks correctly served in a row")]
+        [SerializeField] private int combo = 0;
+
+    [Header("Total Satisfaction")]
+        [Tooltip("Satisfication level (lives)")]
+        [SerializeField] private int totalSatisfaction = 0;
 
     // Update is called once per frame
     void Update()
@@ -22,4 +30,15 @@ public class GameManager : GenericSingleton<GameManager>
         timer += Time.deltaTime;
         secondsElapsed = (int)(timer % 60);
     }
+
+    # region Combo Functions
+    public void ResetComboCounter() { combo = 0; }
+    public void IncreaseComboCounter() { combo++; }
+    #endregion
+
+    # region Satisfaction Functions
+    public void IncreaseSatisfaction(int amount) { totalSatisfaction += amount; }
+    public void DecreaseSatisfaction(int amount) { totalSatisfaction -= amount; }
+    #endregion 
+    
 }
