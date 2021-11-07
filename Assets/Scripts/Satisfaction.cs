a109acc187ef6b5c9db5e569100220be62512267
Do3naut <51458829+Do3naut@users.noreply.github.com>
2021-11-07 01:39:28 -0700

    UI Satisfaction bar and stars

---
+++ Assets/Scripts/Satisfaction.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Satisfaction : MonoBehaviour
+{
+    [SerializeField] RectTransform bar;
+    [SerializeField] Image barColor;
+    [SerializeField] Slider slider;  // Entirely temporary. Used to test the bar
+    [SerializeField] Image star1;
+    [SerializeField] Image star2;
+    [SerializeField] Image star3;
+    [SerializeField] SpriteList sprites;
+
+    float maxSatisfaction = 100;
+    float satisfaction;
+
+    Sprite starLit;
+    Sprite starUnlit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        satisfaction = maxSatisfaction;
+        
+        slider.minValue = 0;
+        slider.maxValue = 100;
+        starLit = sprites.GetSprite("starLit");
+        starUnlit = sprites.GetSprite("starUnlit");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        satisfaction = slider.value;
+        UpdateStars();
+        bar.localScale = new Vector3(Mathf.Lerp(bar.localScale.x, satisfaction / maxSatisfaction, 0.6f), 1);
+    }
+
+    void UpdateStars()
+    {
+        if (satisfaction > 40)
+        {
+            star1.sprite = starLit;
+        } else
+        {
+            star1.sprite = starUnlit;
+        }
+        if (satisfaction > 70)
+        {
+            star2.sprite = starLit;
+        } else
+        {
+            star2.sprite = starUnlit;
+        }
+        if (satisfaction > 95)
+        {
+            star3.sprite = starLit;
+        } else
+        {
+            star3.sprite = starUnlit;
+        }
+    }
+}
