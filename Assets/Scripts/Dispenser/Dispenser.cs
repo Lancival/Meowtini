c269a7417d9d3144b27237bb4ef81bc5881410c0
Peter Sutarjo <peter.sutarjo@gmail.com>
2021-11-07 01:16:51 -0700

    Configured pouring animation

--- Assets/Scripts/Dispenser/Dispenser.cs
+++ Assets/Scripts/Dispenser/Dispenser.cs
@@ -4,51 +4,43 @@ using UnityEngine;
 
 public class Dispenser : MonoBehaviour
 {
-    [Header("Settings")]
-    [Tooltip("DropLocation where Draggable objects can go.")]
-    [SerializeField] private DropLocation dropLocation;
-    
+    [Header("Settings")]    
     [Tooltip("Name of liquid color")]
     [SerializeField] private string liquid;
-
-    //[Tooltip("Levers to dispense liquid.")]
-    //[SerializeField] private Lever[] levers;
-
-
     [SerializeField] private float dispenserRate;
 
-    private Drink drink;
+    private Shaker shaker;
 
-    // Gets called by OnMouseDown() in Lever.cs
+    void Start()
+    {
+        shaker = FindObjectOfType<Shaker>();    
+    }
+
     public void DispenseLiquid()
     {
-        if (dropLocation.isOccupied && dropLocation.draggableReference != null)
-        {   
-            drink = dropLocation.draggableReference.GetComponent<Drink>();
-            if (drink == null)
-            {
-                Debug.LogError("Expecting drink to be occupying a drop location.");
-                return;
-            } 
+        if (shaker == null)
+        {
+            Debug.LogError("Expecting shaker to be occupying a drop location.");
+            return;
+        } 
 
-            // Iterate each base liquid color and calculate how much is currently stored in the shaker
-            float sum = 0;
-            foreach(var key in drink.liquids.Keys)
-            {
-                sum += drink.liquids[key];
-            }   
-            
-            // If drink is at max capacity, don't add more
-            if (sum >= drink.capacity)
-                return;
-            
-            // Check if liquid already exists in dictionary
-            if (drink.liquids.ContainsKey(liquid))
-                drink.liquids[liquid] += dispenserRate;
-            else
-                drink.liquids[liquid] = dispenserRate;
+        // Iterate each base liquid color and calculate how much is currently stored in the shaker
+        float sum = 0;
+        foreach(var key in shaker.liquids.Keys)
+        {
+            sum += shaker.liquids[key];
+        }   
+        
+        // If shaker is at max capacity, don't add more
+        if (sum >= shaker.capacity)
+            return;
+        
+        // Check if liquid already exists in dictionary
+        if (shaker.liquids.ContainsKey(liquid))
+            shaker.liquids[liquid] += dispenserRate;
+        else
+            shaker.liquids[liquid] = dispenserRate;
 
-            drink.curVolume = sum + dispenserRate;
-        }
+        shaker.curVolume = sum + dispenserRate;
     }
 }
---
+++ Assets/Scripts/Shaker.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaker : MonoBehaviour
+{
+    [Header("Shaker Capacity Settings")]
+    public float capacity; // Maximum volume of liquid shaker can hold
+    public float curVolume = 0; // Current volume of liquid stored in the shaker
+    public Dictionary<string, float> liquids;  // String is the liquid name, float is the amount
+    public int numIceCubes = 0; // Number of ice cubes 
+
+    [Header("Timer Settings")]
+    [SerializeField] private Dispenser dispenser;
+    [SerializeField] private float timer;
+    [SerializeField] private float period = 0.5f;
+    [SerializeField] private bool startTimer = false;
+    
+    void Start() {
+        liquids = new Dictionary<string, float>();
+        liquids.Clear();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Ice")
+        {
+            Debug.Log("Feature not implemented");
+        }
+        if (other.gameObject.tag == "Dispenser")
+        {
+            Debug.Log("OnTriggerEnter2D called for the Shaker");
+            dispenser = other.gameObject.GetComponent<Dispenser>();
+            Animator anim = other.gameObject.GetComponent<Animator>();
+            anim.SetBool("Pour", true);
+            startTimer = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag == "Dispenser")
+        {
+            Debug.Log("OnTriggerExit2D called for the Shaker");
+            Animator anim = other.gameObject.GetComponent<Animator>();
+            anim.SetBool("Pour", false);
+            timer = period;
+            dispenser = null;
+            startTimer = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.gameObject.tag == "Dispenser")
+        {
+            if (dispenser != null && startTimer)
+            {
+                timer -= Time.deltaTime;
+                if (timer <= 0)
+                {
+                    dispenser.DispenseLiquid();
+                    timer = period;
+                }      
+            }
+        }    
+    }
+}
