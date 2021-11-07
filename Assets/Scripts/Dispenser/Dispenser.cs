243633ab968e67f2577999f91280c4f01151d8c0
Peter Sutarjo <peter.sutarjo@gmail.com>
2021-11-06 17:16:36 -0700

    drink tag and dispenser added

--- Assets/Scripts/Dispenser/Dispenser.cs
+++ Assets/Scripts/Dispenser/Dispenser.cs
@@ -4,14 +4,53 @@ using UnityEngine;
 
 public class Dispenser : MonoBehaviour
 {
-	[Tooltip("DropLocation where Draggable objects can go.")]
+    [Header("Settings")]
+    [Tooltip("DropLocation where Draggable objects can go.")]
     [SerializeField] private DropLocation dropLocation;
+    
+    [Tooltip("Name of liquid color")]
+    [SerializeField] private string liquid;
 
     //[Tooltip("Levers to dispense liquid.")]
     //[SerializeField] private Lever[] levers;
 
+
+    [SerializeField] private float dispenserRate;
+
+    private Drink drink;
+
+    // Gets called by OnMouseDown() in Lever.cs
     public void DispenseLiquid()
     {
+        if (dropLocation.isOccupied)
+        {
+            if (drink == null)
+            {
+                Debug.LogError("Expecting drink to be occupying a drop location.");
+            } 
+
+            // Iterate each base liquid color and calculate how much is currently stored in the shaker
+            float sum = 0;
+            foreach(KeyValuePair<string, float> entry in drink.liquids)
+            {
+                sum += entry.Value ;
+            }   
+
+            if (sum >= drink.capacity)
+                return;
+            
+            drink.liquids[liquid] += dispenserRate;
+            drink.curVolume = sum + dispenserRate;
+        }
+        
     	Debug.Log("Liquid dispensing functionality not yet implemented.");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Drink" && !dropLocation.isOccupied)
+        {
+            drink = collision.gameObject.GetComponent<Drink>();
+        }
     }
 }
--- Assets/Scripts/Drink.cs
+++ Assets/Scripts/Drink.cs
@@ -12,6 +12,10 @@ public class Drink : MonoBehaviour
     public Dictionary<string, float> liquids;  // String is the liquid name, float is the amount (units undecided)
     public Dictionary<string, int> toppings;  // String is topping name, int is number of said topping
 
+    public float capacity {get; private set;} // Volume of liquid drink can hold
+    public float curVolume {get; set;}
+
+
     void Start()
     {
         
